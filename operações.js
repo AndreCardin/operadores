b41ac8559de1bc2a7cd419310b6f64a1eb5f34f8
AndreCardin <128807208+AndreCardin@users.noreply.github.com>
2024-05-02 12:20:34 -0300

    Add files via upload

---
+++ operações.js
@@ -0,0 +1,39 @@
+//const
+
+const primeiroNumber = 12;
+const segundoNumber = 4;
+
+const operacaoMatematica = primeiroNumber + segundoNumber;
+
+console.log(operacaoMatematica)
+
+const primeirtexto = ' sou'
+const segundtexto = ' André';
+
+const operacaMatematica = primeirtexto + segundtexto;
+
+console.log(operacaMatematica)
+
+//let
+
+let um = 12;
+let dois = 15; 
+
+console. log  ( ops = um + dois )
+
+let um1 = 'sou';
+let dois1 = ' lindo'; 
+
+console. log  ( ops1 = um1 + dois1 )
+
+//var
+
+var maluco = 33;
+var doido = 13;
+
+console. log ( jorge = maluco + doido )
+
+var maluco1 = 'oi';
+var doido1 = ' qual seu nome?';
+
+console. log ( jorge1 = maluco1 + doido1)
